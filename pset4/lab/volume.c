a001e8371cad79daced8427d76be5b9b20a82381
Ahmed Baligh <ahmedm11@hotmail.com>
2020-10-25 14:13:02 +0200

    added full functionality

--- pset4/lab/volume.c
+++ pset4/lab/volume.c
@@ -33,9 +33,20 @@ int main(int argc, char *argv[])
 
     float factor = atof(argv[3]);
 
-    // TODO: Copy header from input file to output file
+    // Copy header from input file to output file
+    uint8_t *header = malloc(sizeof(uint8_t) * HEADER_SIZE);
+    fread(header, sizeof(uint8_t), HEADER_SIZE, input);
+    fwrite(header, sizeof(uint8_t), HEADER_SIZE, output);
+    free(header);
 
-    // TODO: Read samples from input file and write updated data to output file
+    // Read samples from input file and write updated data to output file
+    int16_t sample;
+    while (fread(&sample, sizeof(int16_t), 1, input))
+    {
+        // scale each sample by the factor the user inputed
+        sample *= factor;
+        fwrite(&sample, sizeof(int16_t), 1, output);
+    }
 
     // Close files
     fclose(input);
