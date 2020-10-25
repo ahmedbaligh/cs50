c6ec2cb14d97fb8511ce3344c5195aa8dbd4d12a
Ahmed Baligh <ahmedm11@hotmail.com>
2020-10-25 19:08:47 +0200

    implemented grayscale filter

--- pset4/filter/helpers.c
+++ pset4/filter/helpers.c
@@ -1,9 +1,23 @@
 #include "helpers.h"
+#include <math.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    int grayColor;
+    for (int row = 0; row < height; row++)
+    {
+        for (int col = 0; col < width; col++)
+        {
+            // Get average color value in each pixel to use as its gray shade
+            grayColor = round((image[row][col].rgbtRed + image[row][col].rgbtGreen + image[row][col].rgbtBlue) / 3);
+
+            // Replace each color in each pixel with a shade on the black-white spectrum
+            image[row][col].rgbtRed = grayColor;
+            image[row][col].rgbtGreen = grayColor;
+            image[row][col].rgbtBlue = grayColor;
+        }
+    }
 }
 
 // Convert image to sepia
