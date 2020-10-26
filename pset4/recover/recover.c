f6931de8f88992fbcaeee0dd985cbc5a90cca8a3
Ahmed Baligh <ahmedm11@hotmail.com>
2020-10-27 00:18:00 +0200

    added input and file validations

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+
  
 int main(int argc, char *argv[])
 {
- 
+    // Check command-line arguments
+    if (argc != 2)
+    {
+        printf("Usage: ./recover image\n");
+        return 1;
+    }
+
+    // Open raw file and check for validity
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL)
+    {
+        printf("Could not open file.\n");
+        return 2;
+    }
+
+    // Close files
+    fclose(file);
 }
