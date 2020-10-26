1e68f65b9a7453f8d65119e098cc30d27f31c726
Ahmed Baligh <ahmedm11@hotmail.com>
2020-10-27 00:27:21 +0200

    implemented functionality to detect jpeg images

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
 
+typedef uint8_t BYTE;
  
 int main(int argc, char *argv[])
 {
@@ -19,6 +22,21 @@ int main(int argc, char *argv[])
         return 2;
     }
 
-    // Close files
+    // Read 512-byte chunks of data from the raw file until it's the end of the file
+    BYTE buffer[512];
+    bool isJPEG;
+    int count = 0;
+    while (fread(buffer, 512, 1, file))
+    {
+        // Detect whether this chunk is a jpeg through jpeg header values
+        isJPEG = (buffer[0] == 0xff) && (buffer[1] == 0xd8) && (buffer[2] == 0xff) && ((buffer[3] & 0xf0)  == 0xe0);
+        if (isJPEG)
+        {
+            count++;
+        }
+    }
+    printf("Number: %i\n", count);
+
+    // Close raw file
     fclose(file);
 }
