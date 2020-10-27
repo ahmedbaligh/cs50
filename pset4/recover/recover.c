851dc7bb2db4f5076faacc5b13d59cf1d9881e1c
Ahmed Baligh <ahmedm11@hotmail.com>
2020-10-27 14:41:08 +0200

    complete functionality to recover JPEGs

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+// Define a byte
 typedef uint8_t BYTE;
  
 int main(int argc, char *argv[])
@@ -25,17 +26,49 @@ int main(int argc, char *argv[])
     // Read 512-byte chunks of data from the raw file until it's the end of the file
     BYTE buffer[512];
     bool isJPEG;
-    int count = 0;
+    int imgNum = 0;
+    char *filename;
+    FILE *img = NULL;
     while (fread(buffer, 512, 1, file))
     {
         // Detect whether this chunk is a jpeg through jpeg header values
         isJPEG = (buffer[0] == 0xff) && (buffer[1] == 0xd8) && (buffer[2] == 0xff) && ((buffer[3] & 0xf0)  == 0xe0);
+
+        // If a new jpeg header is detected and a jpeg image is already
+        // open, then close that file and increase the jpeg count
+        if (isJPEG && img != NULL)
+        {
+            fclose(img);
+            imgNum++;
+        }
+
         if (isJPEG)
         {
-            count++;
+            // Format the name of each jpeg image to ###.jpg
+            filename = malloc(3);
+            sprintf(filename, "%03i.jpg", imgNum);
+
+            // Create a jpeg image of correct name and format
+            img = fopen(filename, "w");
+            if (img == NULL)
+            {
+                printf("Could not create %s.\n", filename);
+                return 3;
+            }
+
+            // Release memory resources
+            free(filename);
+        }
+
+        // If there is an open jpeg file, write 512-byte chunk of data into it
+        if (img != NULL)
+        {
+            fwrite(buffer, 512, 1, img);
         }
     }
-    printf("Number: %i\n", count);
+
+    // Close last jpeg file
+    fclose(img);
 
     // Close raw file
     fclose(file);
