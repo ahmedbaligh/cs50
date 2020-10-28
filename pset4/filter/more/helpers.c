94f6d7ac9a9874f5c9662aaf9a24849c0d453e96
Ahmed Baligh <ahmedm11@hotmail.com>
2020-10-28 15:40:05 +0200

    implemented Edges filter

--- pset4/filter/more/helpers.c
+++ pset4/filter/more/helpers.c
@@ -1,6 +1,12 @@
 #include "helpers.h"
 #include <math.h>
 
+// Function Prototypes
+int *calculateSobel(RGBTRIPLE grid[]);
+
+// Define convolutional matrices
+const int Gx[9] = {-1, 0, 1, -2, 0, 2, -1, 0, 1};
+const int Gy[9] = {-1, -2, -1, 0, 0, 0, 1, 2, 1};
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
@@ -172,5 +178,152 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    // Variables to hold the pixels in the 3x3 grid
+    RGBTRIPLE upperLeft, upper, upperRight, left, middle, right, bottomLeft, bottom, bottomRight;
+
+    // Define zero values to ignore pixels that are beyond the borders
+    RGBTRIPLE ignore;
+    ignore.rgbtRed = ignore.rgbtGreen = ignore.rgbtBlue = 0;
+
+    // Array to hold the all the pixels with Edges filter
+    RGBTRIPLE edged[height][width];
+
+    // Pointer to returned array with Sobel magnitudes
+    int *sobel;
+
+    for (int row = 0; row < height; row++)
+    {
+        for (int col = 0; col < width; col++)
+        {
+            // Store target pixel and its surronding pixels in the
+            // 3x3 grid based on its position in the image array
+            middle = image[row][col];
+            upperLeft = upper = upperRight = left = right = bottomLeft = bottom = bottomRight = ignore;
+
+            // Corner case: top-left
+            if (row == 0 && col == 0)
+            {
+                right = image[row][col + 1];
+                bottom = image[row + 1][col];
+                bottomRight = image[row + 1][col + 1];
+            }
+            // Corner case: bottom-left
+            else if (row == height - 1 && col == 0)
+            {
+                upper = image[row - 1][col];
+                upperRight = image[row - 1][col + 1];
+                right = image[row][col + 1];
+            }
+            // Corner case: top-right
+            else if (row == 0 && col == width - 1)
+            {
+                left = image[row][col - 1];
+                bottomLeft = image[row + 1][col - 1];
+                bottom = image[row + 1][col];
+            }
+            // Corner case: bottom-right
+            else if (row == height - 1 && col == width - 1)
+            {
+                upperLeft = image[row - 1][col - 1];
+                upper = image[row - 1][col];
+                left = image[row][col - 1];
+            }
+            // Edge case: top
+            else if (row == 0)
+            {
+                left = image[row][col - 1];
+                right = image[row][col + 1];
+                bottomLeft = image[row + 1][col - 1];
+                bottom = image[row + 1][col];
+                bottomRight = image[row + 1][col + 1];
+            }
+            // Edge case: left
+            else if (col == 0)
+            {
+                upper = image[row - 1][col];
+                upperRight = image[row - 1][col + 1];
+                right = image[row][col + 1];
+                bottom = image[row + 1][col];
+                bottomRight = image[row + 1][col + 1];
+            }
+            // Edge case: bottom
+            else if (row == height - 1)
+            {
+                upperLeft = image[row - 1][col - 1];
+                upper = image[row - 1][col];
+                upperRight = image[row - 1][col + 1];
+                left = image[row][col - 1];
+                right = image[row][col + 1];
+            }
+            // Edge case: right
+            else if (col == width - 1)
+            {
+                upperLeft = image[row - 1][col - 1];
+                upper = image[row - 1][col];
+                left = image[row][col - 1];
+                bottomLeft = image[row + 1][col - 1];
+                bottom = image[row + 1][col];
+            }
+            // Regular (middle) case
+            else
+            {
+                upperLeft = image[row - 1][col - 1];
+                upper = image[row - 1][col];
+                upperRight = image[row - 1][col + 1];
+                left = image[row][col - 1];
+                right = image[row][col + 1];
+                bottomLeft = image[row + 1][col - 1];
+                bottom = image[row + 1][col];
+                bottomRight = image[row + 1][col + 1];
+            }
+
+            // Store 3x3 grid for each pixel into an array to calculate Sobel magnitudes
+            RGBTRIPLE grid[9] = {upperLeft, upper, upperRight, left, middle, right, bottomLeft, bottom, bottomRight};
+            
+            // Calculate the Sobel magnitude for each color
+            sobel = calculateSobel(grid);
+            edged[row][col].rgbtRed = *(sobel + 0);
+            edged[row][col].rgbtGreen = *(sobel + 1);
+            edged[row][col].rgbtBlue = *(sobel + 2);
+        }
+    }
+
+    // Store the pixels of the edged image in the original image 
+    for (int row = 0; row < height; row++)
+    {
+        for (int col = 0; col < width; col++)
+        {
+            image[row][col] = edged[row][col];
+        }
+    }
+}
+
+// Calculate edges based on Sobel operator
+int *calculateSobel(RGBTRIPLE grid[]) {
+    int sumXRed, sumYRed, sumXGreen, sumYGreen, sumXBlue, sumYBlue;
+    sumXRed = sumYRed = sumXGreen = sumYGreen = sumXBlue = sumYBlue = 0; 
+
+    // Calculate Gx and Gy for each color
+    for (int i = 0; i < 9; i++) {
+        sumXRed += (grid[i].rgbtRed * Gx[i]);
+        sumYRed += (grid[i].rgbtRed * Gy[i]);
+        sumXGreen += (grid[i].rgbtGreen * Gx[i]);
+        sumYGreen += (grid[i].rgbtGreen * Gy[i]);
+        sumXBlue += (grid[i].rgbtBlue * Gx[i]);
+        sumYBlue += (grid[i].rgbtBlue * Gy[i]);
+    }
+
+    static int magnitude[3];
+
+    // Calculate the gradient magnitude for each color
+    int magnitudeRed = round(sqrt(pow(sumXRed, 2) + pow(sumYRed, 2)));
+    int magnitudeGreen = round(sqrt(pow(sumXGreen, 2) + pow(sumYGreen, 2)));
+    int magnitudeBlue = round(sqrt(pow(sumXBlue, 2) + pow(sumYBlue, 2)));
+
+    // Cap the values of colors at 255 and store them in array of magnitudes
+    magnitude[0] = (magnitudeRed < 255) ? magnitudeRed : 255;
+    magnitude[1] = (magnitudeGreen < 255) ? magnitudeGreen : 255;
+    magnitude[2] = (magnitudeBlue < 255) ? magnitudeBlue : 255;
+
+    return magnitude;
 }
