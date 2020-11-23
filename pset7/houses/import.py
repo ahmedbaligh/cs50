0e97a4e50406312af730d86bd2487b90ae772592
Ahmed Baligh <ahmedm11@hotmail.com>
2020-11-23 04:43:32 +0200

    implemented scripts to import data into database

--- pset7/houses/import.py
+++ pset7/houses/import.py
@@ -1,1 +1,36 @@
-# TODO
+import csv
+from sys import argv, exit
+from cs50 import SQL
+
+
+# Ensure correct usage
+if len(argv) != 2:
+    exit('Usage: python import.py data.csv')
+
+# Open database
+db = SQL("sqlite:///students.db")
+
+# Read csv file of students data into students database
+with open(argv[1], 'r') as file:
+    reader = csv.DictReader(file)
+
+    for row in reader:
+        # Split names into first, middle, and last names
+        name = row['name'].split(' ')
+
+        if len(name) == 3:
+            middle_name = name[1]
+            last_name = name[2]
+        # Leave middle name as Null, if student does not have one
+        else:
+            middle_name = None
+            last_name = name[1]
+
+        # Insert each student's data into the database
+        db.execute('''
+            INSERT INTO 
+            students (first, middle, last, house, birth)
+            VALUES(?, ?, ?, ?, ?)''',
+            name[0], middle_name, last_name, row['house'], int(row['birth'])
+        )
+        
