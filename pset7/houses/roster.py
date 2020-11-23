47f772759344cffd6985a3b690163689c118ec36
Ahmed Baligh <ahmedm11@hotmail.com>
2020-11-23 05:33:22 +0200

    implemented data roster query of inputed house

--- pset7/houses/roster.py
+++ pset7/houses/roster.py
@@ -1,1 +1,28 @@
-# TODO
+import csv
+from sys import argv, exit
+from cs50 import SQL
+
+
+# Ensure correct usage
+if len(argv) != 2:
+    exit('Usage: python roster.py house_name')
+
+# Open database
+db = SQL("sqlite:///students.db")
+
+# Query students of the provided house name
+students = db.execute('''
+                        SELECT first, middle, last, birth
+                        FROM students
+                        WHERE house = ?
+                        ORDER BY last, first''',
+                        argv[1]
+                    )
+
+# Print a roster of students from specified house
+for student in students:
+    # Check for the existence of a middle name
+    if student['middle'] is None:
+        print(f"{student['first']} {student['last']}, born {student['birth']}")
+    else:
+        print(f"{student['first']} {student['middle']} {student['last']}, born {student['birth']}")
