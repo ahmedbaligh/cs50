022c3d006f2b119929c7b0cbd0022ec717292a7d
Ahmed Baligh <ahmedm11@hotmail.com>
2020-12-28 22:32:25 +0200

    implemented full functionality

--- pset9/lab/application.py
+++ pset9/lab/application.py
@@ -15,15 +15,20 @@ db = SQL("sqlite:///birthdays.db")
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
+        # Add the user's entry into the database
+        name = request.form.get('name')
+        month = request.form.get('month')
+        day = request.form.get('day')
 
-        # TODO: Add the user's entry into the database
+        if not name or not month or not day:
+            return
+
+        db.execute('INSERT INTO birthdays(name, month, day) VALUES(?, ?, ?)', name, month, day)
 
         return redirect("/")
 
     else:
+        # Display the entries in the database on index.html
+        people = db.execute('SELECT * FROM birthdays')
 
-        # TODO: Display the entries in the database on index.html
-
-        return render_template("index.html")
-
-
+        return render_template("index.html", people=people)
